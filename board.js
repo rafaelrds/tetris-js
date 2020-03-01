1cabde987d1137a491997a3fe446043dc5d8e659
Rafael Rego <crafards@gmail.com>
2020-03-01 19:53:25 -0300

    add play function

---
+++ board.js
@@ -0,0 +1,15 @@
+class Board {
+  grid;
+  
+  // Reset the board when we start a new game.
+  reset() {
+    this.grid = this.getEmptyBoard();
+  }
+  
+  // Get matrix filled with zeros.
+  getEmptyBoard() {
+    return Array.from(
+      {length: ROWS}, () => Array(COLS).fill(0)
+    );
+  }
+}
--- main.js
+++ main.js
@@ -7,3 +7,11 @@ ctx.canvas.height = ROWS * BLOCK_SIZE;
 
 // Scale blocks
 ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
+
+
+let board = new Board();
+
+function play() {
+    board.reset();
+    console.table(board.grid);
+}
